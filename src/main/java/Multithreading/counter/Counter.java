1f43f63619b26bdd67a074b87b882ba918ebcf4a
avasyukhin <avasyukhin@inbox.ru>
2016-06-02 16:39:08 +0400

    Fixed counter

--- src/main/java/Multithreading/counter/Counter.java
+++ src/main/java/Multithreading/counter/Counter.java
@@ -4,11 +4,13 @@ package Multithreading.counter;
  * Created by Aphex on 24.05.2016.
  */
 import java.lang.Thread;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter extends Thread {
-    static volatile int counter;
+    public static AtomicInteger counter;
     public void run() {
         for (int i = 0; i< 100000; i++){
-            counter++;
+            counter.compareAndSet(counter.get(),counter.addAndGet(1));
         }
     }
 }
--- src/main/java/Multithreading/counter/Runner.java
+++ src/main/java/Multithreading/counter/Runner.java
@@ -1,16 +1,23 @@
 package Multithreading.counter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by Aphex on 02.06.2016.
  */
-public class Runner {
-    public static void execute(){
-        Counter.counter=0;
+public class Runner{
+    public static void execute (){
+        Counter.counter=new AtomicInteger(0);
         Counter[] counters = new Counter[100];
         for (Counter counter:counters){
             counter = new Counter();
             counter.start();
+            try {
+                counter.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.print("counter = "+Counter.counter);
+        System.out.print("counter = " + Counter.counter);
     }
 }
